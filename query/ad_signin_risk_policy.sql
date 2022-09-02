2dcde69253118d592584c294c28768807b6a35d0
Priyanka Chatterjee <priyanka.chatterjee@turbot.com>
2022-09-02 16:44:50 +0530

    add office365 section1 queries(wip) (#5)

---
+++ query/ad_signin_risk_policy.sql
@@ -0,0 +1,32 @@
+with block_legacy_authentication as (
+select 
+  tenant_id,
+  count(*)
+from 
+  azuread_conditional_access_policy
+where
+  users->'includeUsers' ?& array['All'] and
+  jsonb_array_length(users -> 'excludeUsers') = 0 and
+  jsonb_array_length(sign_in_risk_levels) != 0 and
+  applications->'includeApplications' ?& array['All'] and
+  jsonb_array_length(applications -> 'excludeApplications') = 0 and
+  built_in_controls ?& array['mfa']
+  -- and state="enabled"  
+  group by tenant_id
+),
+tenant_list as (
+  select distinct on (tenant_id) tenant_id from azuread_user;
+)
+select
+  tenant_id as resource,
+  case
+    when (select count from block_legacy_authentication where tenant_id = t.tenant_id) > 0 then 'ok'
+    else 'alarm'
+  end as status,
+  case
+    when (select count from block_legacy_authentication where tenant_id = t.tenant_id) > 0 then  'sign-in risk policies enabled.'
+    else 'sign-in risk policies disabled.'
+  end as reason,
+  tenant_id as tenant_id
+from
+  tenant_list as t;
