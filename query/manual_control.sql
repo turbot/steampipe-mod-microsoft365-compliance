55d719da13a6120d5b20f56b6967d40adddcef61
rajmohanty17 <raj@turbot.com>
2022-09-02 20:17:23 +0530

    Add manual  template query

---
+++ query/manual_control.sql
@@ -0,0 +1,9 @@
+select
+  -- Required Columns
+   tenant_id as resource,
+  'info' as status,
+  'Manual verification required.' as reason,
+  -- Additional Dimensions
+  display_name
+from
+  azure_tenant;
