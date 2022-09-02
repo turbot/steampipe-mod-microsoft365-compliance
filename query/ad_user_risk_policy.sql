8e37d13cace9125e89eb303e41638e5e909d9ebd
Priyanka-Chatterjee-2000 <priyankachatterjee9075@gmail.com>
2022-09-02 11:21:41 +0530

    section 1.1.8 and section 1.1.9 queries pushed

---
+++ query/ad_user_risk_policy.sql
@@ -0,0 +1,33 @@
+with block_legacy_authentication as (
+select 
+    tenant_id,
+    count(*)
+from 
+    azuread_conditional_access_policy
+where
+    users->'includeUsers' ?& array['All'] and
+    jsonb_array_length(users->'excludeUsers') = 0 and
+    jsonb_array_length(user_risk_levels) != 0 and
+    applications->'includeApplications' ?& array['All'] and
+    jsonb_array_length(applications->'excludeApplications') = 0 and
+    built_in_controls ?& array['passwordChange']
+    -- and state="enabled"  
+    group by tenant_id
+),
+
+tenant_list as (
+  select distinct on (tenant_id) tenant_id from azuread_user
+)
+select
+  tenant_id as resource,
+  case
+    when (select count from block_legacy_authentication where tenant_id = t.tenant_id) > 0 then 'ok'
+    else 'alarm'
+  end as status,
+  case
+    when (select count from block_legacy_authentication where tenant_id = t.tenant_id) > 0 then t.tenant_id || 'have sign-in risk policies enabled.'
+    else t.tenant_id || 'doesnt have any sign-in risk policies enabled.'
+  end as reason,
+  tenant_id as tenant_id
+from
+  tenant_list as t;
