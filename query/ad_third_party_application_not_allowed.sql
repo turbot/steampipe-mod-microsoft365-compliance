e5f4ad31d8aa99e0612dfa02520688f2d8df2037
niharika2k00 <niharika@turbot.com>
2022-09-06 17:36:23 +0530

    resolve the error

--- query/ad_third_party_application_not_allowed.sql
+++ query/ad_third_party_application_not_allowed.sql
@@ -3,11 +3,11 @@ select
   -- Required columns
   tenant_id as resource,
   case
-    when default_user_role_permissions ->> 'allowedToCreateApps' = 'false' then 'ok'
+    when not (default_user_role_permissions -> 'allowedToCreateApps')::bool then 'ok'
     else 'alarm'
   end as status,
   case
-     when default_user_role_permissions ->> 'allowedToCreateApps' = 'false' then 'third party integrated applications are not allowed.'
+     when not (default_user_role_permissions -> 'allowedToCreateApps')::bool then 'third party integrated applications are not allowed.'
      else 'third party integrated applications are allowed.'
   end as reason,   
   -- Additional Dimensions
--- query/office365_domain_sharing_restriction_mode.sql
+++ query/office365_domain_sharing_restriction_mode.sql
@@ -3,11 +3,11 @@ select
   -- Required columns
   tenant_id as resource,
   case
-    when sharing_domain_restriction_mode  = 'allowList' or sharing_domain_restriction_mode = 'blockList.' then 'ok'
+    when sharing_domain_restriction_mode  = 'allowList' or sharing_domain_restriction_mode = 'blockList' then 'ok'
     else 'alarm'
   end as status,
   case
-     when sharing_domain_restriction_mode  = 'allowList' or sharing_domain_restriction_mode = 'blockList.' then 'document sharing is being controlled by domains with blacklist or whitelist.'
+     when sharing_domain_restriction_mode  = 'allowList' or sharing_domain_restriction_mode = 'blockList' then 'document sharing is being controlled by domains with blacklist or whitelist.'
      else 'document sharing is not being controlled by domains with blacklist or whitelist.'
   end as reason,   
   -- Additional Dimensions
