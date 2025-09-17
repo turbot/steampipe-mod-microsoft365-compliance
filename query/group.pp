33bea33fb5ee6a44d521c2011281723b99e6e9e8
khushboosharma <khushboo@turbot.com>
2025-09-17 21:54:37 +0530

    add new query

--- query/group.pp
+++ query/group.pp
@@ -16,3 +16,40 @@ query "azuread_group_not_public" {
       azuread_group;
   EOQ
 }
+
+query "azuread_dynamic_group_for_guest_user" {
+  sql = <<-EOQ
+    with tenant_list as (
+      select
+        distinct on (tenant_id) tenant_id
+      from
+        azuread_user
+    ), dynamic_group_for_guest_user as (
+      select
+        count(*) as dynamic_group_for_guest_user_count,
+        tenant_id
+      from
+        azuread_group
+      where
+        membership_rule = '(user.userType -eq "guest")'
+        and group_types @> '[ "DynamicMembership" ]'
+      group by
+        tenant_id
+    )
+    select
+      t.tenant_id as resource,
+      case
+        when dynamic_group_for_guest_user_count > 0 then 'ok'
+        else 'alarm'
+      end status,
+      case
+        when dynamic_group_for_guest_user_count > 0 then t.tenant_id || ' has dynamic group for guest user.'
+        else t.tenant_id || ' does not have dynamic group for guest user.'
+      end reason
+      -- ${local.tag_dimensions_sql}
+      -- ${local.common_dimensions_sql}
+    from
+      tenant_list as t
+      left join dynamic_group_for_guest_user as d on d.tenant_id = t.tenant_id
+  EOQ
+}
